56c5d3bc0123d0d1ece7ca951a5e6a4a8d7ff6bf
Zitong Mao <njsxmzt@gmail.com>
2017-12-05 15:36:49 +0900

    fixed indentation error

--- gomoku_ai.py
+++ gomoku_ai.py
@@ -22,36 +22,36 @@ class gomokuAI(object):
 				return True
 		return False
 
-    def evaluate(self):
-        vectors = []
+	def evaluate(self):
+		vectors = []
 
-        for i in xrange(N):
-            vectors.append(self.__gomoku.get_chessMap()[i])
-            
-        for j in xrange(N):
-            vectors.append([self.__gomoku.get_chessMap()[i][j] for i in range(N)])
+		for i in xrange(N):
+		    vectors.append(self.__gomoku.get_chessMap()[i])
 
-        vectors.append([self.__gomoku.get_chessMap()[x][x] for x in range(N)])
-        for i in xrange(1, N-4):
-            v = [self.__gomoku.get_chessMap()[x][x-i] for x in range(i, N)]
-            vectors.append(v)
-            v = [self.__gomoku.get_chessMap()[y-i][y] for y in range(i, N)]
-            vectors.append(v)
+		for j in xrange(N):
+		    vectors.append([self.__gomoku.get_chessMap()[i][j] for i in range(N)])
 
-        vectors.append([self.__gomoku.get_chessMap()[x][N-x-1] for x in range(N)])
-        for i in xrange(4, N-1):
-            v = [self.__gomoku.get_chessMap()[x][i-x] for x in xrange(i, -1, -1)]
-            vectors.append(v)
-            v = [self.__gomoku.get_chessMap()[x][N-x+N-i-2] for x in xrange(N-i-1, N)]
-            vectors.append(v)
+		vectors.append([self.__gomoku.get_chessMap()[x][x] for x in range(N)])
+		for i in xrange(1, N-4):
+		    v = [self.__gomoku.get_chessMap()[x][x-i] for x in range(i, N)]
+		    vectors.append(v)
+		    v = [self.__gomoku.get_chessMap()[y-i][y] for y in range(i, N)]
+		    vectors.append(v)
 
-        board_score = 0
+		vectors.append([self.__gomoku.get_chessMap()[x][N-x-1] for x in range(N)])
+		for i in xrange(4, N-1):
+		    v = [self.__gomoku.get_chessMap()[x][i-x] for x in xrange(i, -1, -1)]
+		    vectors.append(v)
+		    v = [self.__gomoku.get_chessMap()[x][N-x+N-i-2] for x in xrange(N-i-1, N)]
+		    vectors.append(v)
 
-        for v in vectors:
-        	score = evaluate_vector(v)
-        	if self.__currentState == BoardState.WHITE:
-        		board_score += score['white'] - score['black']
-        	else:
-        		board_score += score['black'] - score['white']
+		board_score = 0
 
-        return board_score
+		for v in vectors:
+			score = evaluate_vector(v)
+			if self.__currentState == BoardState.WHITE:
+				board_score += score['white'] - score['black']
+			else:
+				board_score += score['black'] - score['white']
+
+		return board_score
